477643e8a3d5e8cb60090bbbd9021e417e11bee3
Abdulhamid Albaz <a.hamidelbaz@gmail.com>
2023-02-21 15:06:41 +0100

    use streamlit  instead of flask

--- deployment/app.py
+++ deployment/app.py
@@ -1,20 +1,44 @@
-# import necessary libraries:
 import pandas as pd
 import joblib
-from flask import Flask, request,jsonify
+import streamlit as st
 
-#Creat local api using Flask:
-
-app = Flask(__name__)
-
+# Load the model
 load_model = joblib.load('price_prediction_model.sav', mmap_mode ='r')
 
-@app.route('/api',methods=['POST'])
-def price_prediction():
-    data = request.form
+# Define the predict function
+def predict_price(data):
     df = pd.DataFrame([data])
     preds = load_model.predict(df)
-    return jsonify({'prediction': preds[0]})
+    return preds[0]
 
+# Define the Streamlit app
+def app():
+    # Set the app title
+    st.title('Real Estate Price Prediction')
+
+    # Add input fields for the user to enter data
+    province = st.selectbox('Province', ['Brussel','Antwerp','Brabant','West Flanders','East Flanders','Hainaut','Liège','Limburg','Namur'])
+    zip_code = st.number_input('postal Code')
+    building_type = st.selectbox('Building type', ['Apartment', 'House', 'Other'])
+    bedroom = st.number_input('Number of rooms')
+    land_surface = st.number_input('Land surface')
+    heating_type = st.selectbox('Heating type', ['Gas', 'Electric', 'Other'])
+    construction_year = st.number_input('Year of construction')
+
+    # When the user clicks the 'Predict' button, run the predict_price function
+    if st.button('Predict'):
+        data = {
+            'province': province.lower(),
+            'zip': zip_code,
+            'building_type': building_type.lower(),
+            'bedroom': bedroom,
+            'land_surface': land_surface,
+            'heating_type': heating_type.lower(),
+            'construction_Year': construction_year,
+        }
+        prediction = predict_price(data)
+        st.success(f'The predicted price is €{prediction:.2f}')
+
+# Run the app
 if __name__ == '__main__':
-    app.run(port=5000)
+    app()
